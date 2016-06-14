da86bf71c26bcbe5677063d8674c0a1be1522255
netstorm <netstorm@info@cavisson.com>
2016-06-14 18:26:46 +0530

    complete commit

--- routes/manyTier_yahoo.js
+++ routes/manyTier_yahoo.js
@@ -5,8 +5,11 @@
 
 var express = require ('express');
 var router = express.Router();
+var pg = require('pg');
 
 var http = require('http');
+var f1 = false;
+var f2 = false;
 
 function availabilityCheck(req, res)
 {
@@ -24,7 +27,9 @@ function availabilityCheck(req, res)
         response.on('end', function () {
             console.log("Yahoo call has ended");
 
-            respForCallout();
+            f1 = true;
+            if(f1 ==true && f2 ==true)
+                respForCallout();
 
         });
     }
@@ -35,8 +40,51 @@ function availabilityCheck(req, res)
         console.log(err)
     }).end();
 
+    var URL = "postgres://dqlwzcsbobhcci:Lcm2mB5bUamVHB6FiiYWw1Jdkc@ec2-54-221-253-117.compute-1.amazonaws.com:5432/d935m16il25m65";
+    pg.defaults.ssl = true;
+
+    var client = new pg.Client(URL);
+    client.connect();
+    var query = client.query("SELECT * FROM emp;");
+    query.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    query.on("end", function (result) {
+        client.end();
+
+        console.log(JSON.stringify(result.rows, null, " ") + "\n");
+        f2=true;
+        if(f1==true && f2==true)
+            respForCallout();
+    });
+    /*pg.connect(URL,function(err,client)
+    {
+        if(err) throw err;
+
+        console.log("Connected successfully to : "+URL);
+
+        client
+            .query('SELECT * FROM emp;')
+            .on('row',function(data)
+            {
+                console.log(JSON.stringify(data));
+            })
+            .on('end',function()
+            {
+                client.end();
+                /!*f2 ==true ;
+                if(f1 ==true && f2==true)*!/
+                    respForCallout();
+            })
+            .on('err', function (err) {
+                console.log(err);
+            })
+    });*/
+
     function respForCallout() {
-        res.render('manyTier_yahoo', {title: 'Successfully called to Yahoo.com .'});
+        res.render('manyTier_yahoo', {title: 'Successfully Connected to PostGres DB & called to Yahoo.com .'});
+        f1 = false;
+        f2 = false;
     };
 }
 
