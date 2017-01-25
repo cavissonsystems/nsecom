93ed18a0f717bfa523e9b4e5ee77eaa9ff47130e
netstorm <netstorm@info@cavisson.com>
2017-01-25 12:52:08 +0530

    mongodb pool changes

--- model/db.js
+++ model/db.js
@@ -1,10 +1,14 @@
 var mongoose = require('mongoose');
 try {
     var monoDblink = 'mongodb://localhost/nsecomDb';
-    var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-        replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+   /* var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+        replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };*/
 
-    mongoose.connect(monoDblink,options)
+    var options={ server : {reconnectTries: Number.MAX_VALUE,poolSize: 10,socketOptions: { keepAlive: 10000, connectTimeoutMS: 30000 }}}
+
+    mongoose.connect(monoDblink,options);
+
+
 
     var con = mongoose.connection;
     con.on('error',function(err){
